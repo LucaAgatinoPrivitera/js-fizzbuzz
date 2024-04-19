baa2d88e8d991f8cc2859f4847d4ea3564958d41
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-04-19 15:58:04 +0200

    Tentativo di modifica della pagina

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-for(let i=1; i<=100; i++ ){
+// Dichiaro variabile i (contatore)
+let i = 1
+
+//Ciclo che funziona, ma solo in console
+for(i; i<=100; i++ ){
 if(i%5 ==0){
     console.log("ciao");
 }
@@ -9,3 +13,10 @@ else{
     console.log(i);
 }
 }
+
+// Tentativo di modifica della pagina
+const contenitore = document.getElementById("container_numbers");
+console.log(contenitore);
+let newLi = document.createElement("li");
+contenitore.innerHTML = contenitore.innerHTML + (newLi);
+contenitore.createElement(i);
