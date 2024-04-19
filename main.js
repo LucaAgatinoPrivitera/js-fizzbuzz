4bf852f508ae2b3b12a93f195d6abc4e44cab52d
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-04-19 16:31:45 +0200

    commit

--- main.js
+++ main.js
@@ -4,19 +4,27 @@ let i = 1
 //Ciclo che funziona, ma solo in console
 for(i; i<=100; i++ ){
 if(i%5 ==0){
-    console.log("ciao");
+    console.log("buzz");
 }
 else if(i%3==0){
-    console.log("m3");
+    console.log("fizz");
 }
-else{
-    console.log(i);
+else if(i%3==0 && i%5==0){
+    console.log(fizzbuzz);
 }
 }
 
 // Tentativo di modifica della pagina
 const contenitore = document.getElementById("container_numbers");
 console.log(contenitore);
-let newLi = document.createElement("li");
-contenitore.innerHTML = contenitore.innerHTML + (newLi);
-contenitore.createElement(i);
+// contenitore.innerHTML = contenitore.innerHTML + (newLi);
+// contenitore.createElement(i);
+contenitore.innerHTML = contenitore.innerHTML + `<div class="container_i"> <span>${i}</span> </div>`;
+/*
+const ciao = 8;
+const ticketPrice = document.getElementById("container_numbers")
+ticketPrice.appendChild = ("ciao");
+
+let prova = document.getElementById("container_numbers")
+prova.innerHTML = "contenuto che voglio stampare dentro un tag p";
+*/
